a77a46d74ae041558da52f8b1e8fb65a204fb9b1
debug-node <anupraj.2085@gmail.com>
2025-12-02 22:34:04 +0530

    Add: Chapter 1 → 2_Variable.js

---
+++ Practices & Exercises/Chapter Exercise/Chapter 1/2_Variable.js
@@ -0,0 +1,20 @@
+console.log("Variable in JavaScript")
+
+// This is a single line comment
+console.log("Hello World"); // This is also a single line comment
+
+/*
+This is 
+a multi-line 
+comment.
+*/
+
+
+var a = 67 // a contains 67
+console.log(a)
+a = "harry"
+console.log(a)
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7 // Not allowed as var is a reserved keyword
+let harry8 = 9 // Allowed
+console.log(harry8)
