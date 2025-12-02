7cf8898a5267c31459daa6ee9a7169cd22eabc4c
debug-node <anupraj.2085@gmail.com>
2025-12-02 22:33:51 +0530

    Add: Chapter 1 → 1_Intro.js

---
+++ Practices & Exercises/Chapter Exercise/Chapter 1/1_Intro.js
@@ -0,0 +1,8 @@
+console.log("Introduction to JavaScript")
+console.log("This program adds 1, 2 and 54")
+
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+let c = addThreeNumbers(1, 2, 54)
+console.log(c)
