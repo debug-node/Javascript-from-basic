24d7d970d53ff13352ccbda4d77009f6b1d46259
debug-node <anupraj.2085@gmail.com>
2025-12-02 23:00:32 +0530

    Add Chapter 2: 2_UserInput.js

---
+++ Practices & Exercises/Chapter Exercise/Chapter 2/2_UserInput.js
@@ -0,0 +1,14 @@
+console.log("Taking User Input in JavaScript")
+
+let age = prompt("Enter your age");
+console.log(typeof age); //age is of string datatype
+
+let age1 = prompt("Enter your age");
+console.log(typeof age1); //age is of string datatype
+age1 = Number(age1);
+console.log(typeof age1); //age is of number datatype
+
+let age3 = prompt("Enter your age");
+console.log(typeof age3); //age is of string datatype
+age3 = parseInt(age3);
+console.log(typeof age3); //age is of number datatype
