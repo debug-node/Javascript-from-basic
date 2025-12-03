63ea556125e5bc0eadac0060622b0ea66f2e32be
debug-node <anupraj.2085@gmail.com>
2025-12-03 10:41:22 +0530

    Add Chapter 3: 2_While Loops.js

---
+++ Practices & Exercises/Chapter Exercise/Chapter 3/2_While Loops.js
@@ -0,0 +1,20 @@
+console.log("While Loops in JavaScript");
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+// while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i < n) 
