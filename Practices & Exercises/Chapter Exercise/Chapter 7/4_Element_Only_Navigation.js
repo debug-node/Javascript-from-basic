07f6a385e66ab6a7198c530c0b42aadd6d3a9444
debug-node <anupraj.2085@gmail.com>
2025-12-06 22:17:36 +0530

    Add Chapter 7: 4_Element_Only_Navigation.js

---
+++ Practices & Exercises/Chapter Exercise/Chapter 7/4_Element_Only_Navigation.js
@@ -0,0 +1,9 @@
+console.log("Element only navigation in JS");
+
+const changeBgRed = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+
+let b = document.body
+console.log("First child of b is: ", b.firstChild)
+console.log("First Element child of b is: ", b.firstElementChild)
