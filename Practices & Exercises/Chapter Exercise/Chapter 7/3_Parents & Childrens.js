f374778505ef413683ed53b046329fae02a95c51
debug-node <anupraj.2085@gmail.com>
2025-12-06 22:17:24 +0530

    Add Chapter 7: 3_Parents & Childrens.js

---
+++ Practices & Exercises/Chapter Exercise/Chapter 7/3_Parents & Childrens.js
@@ -0,0 +1,7 @@
+console.log("Parent & Sibling in JS");
+
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
